d5c1f5c5af0a1404d131681a26359b41b95d9f44
Angeline Loh <angeline.loh@orionhealth.com>
2020-07-29 11:46:48 +1200

    Update release dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports.register = (program) => {
 
   program
-      .command('release-version', 'Release a version of the project and update all dependent projects');
+      .command('release-version', 'Release a version of the project and update all dependent projects')
+      .command('release-dependent-version', 'Update and release dependent projects');
 }
